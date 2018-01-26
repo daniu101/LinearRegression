08cac614d36aeff111b3df526d4ffe16c27236e2
Administrator <Administrator@PC-20170518KDMT>
2018-01-26 17:35:34 +0800

    更新

--- LR/console_model.py
+++ LR/console_model.py
@@ -9,10 +9,9 @@ from LR.matplotlib_model import matplotlib_show
 from LR.database_model import write_data
 
 DIR_DATASET = "D:/LR.txt"
-
 DIR_DATABASE = "D:/LR_Database.txt"
 
-p0=[100,2]
+p0=[100,2] #存放w、b初始值，可任意制定
 x,y = load_data(DIR_DATASET)
 Xi=np.array(x)
 Yi=np.array(y)
